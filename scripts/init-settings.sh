18fa69dfda0e9327bd315584d981433b72504544
masjeho2 <126146946+masjeho2@users.noreply.github.com>
2024-05-14 19:14:54 +0700

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -74,16 +74,16 @@ phpindexfix () {
 ## patch ui openclash
 clientui_path="/usr/lib/lua/luci/model/cbi/openclash/client.lua"
 patchuiopenclash () {
-    sed -i "101s|^|-- |" ${clientui_path}
-    sed -i "131s|^|-- |" ${clientui_path}
-    sed -i "132s|^|-- |" ${clientui_path}
-    sed -i "133s|^|-- |" ${clientui_path}
-    sed -i "134s|^|-- |" ${clientui_path}
-    sed -i "135s|^|-- |" ${clientui_path}
-    sed -i "137s|^|-- |" ${clientui_path}
-    sed -i "138s|^|-- |" ${clientui_path}
-    sed -i "139s|^|-- |" ${clientui_path}
-    sed -i "140s|^|-- |" ${clientui_path}
+ #   sed -i "101s|^|-- |" ${clientui_path}
+ #   sed -i "131s|^|-- |" ${clientui_path}
+ #   sed -i "132s|^|-- |" ${clientui_path}
+ #   sed -i "133s|^|-- |" ${clientui_path}
+  #  sed -i "134s|^|-- |" ${clientui_path}
+  #  sed -i "135s|^|-- |" ${clientui_path}
+  #  sed -i "137s|^|-- |" ${clientui_path}
+  #  sed -i "138s|^|-- |" ${clientui_path}
+  #  sed -i "139s|^|-- |" ${clientui_path}
+  #  sed -i "140s|^|-- |" ${clientui_path}
 }
 
 ## hide header name
