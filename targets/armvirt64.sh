55fd337221564c44f97384bab073e76e0adfc10c
masjeho2 <126146946+masjeho2@users.noreply.github.com>
2024-05-14 12:57:42 +0700

    Update armvirt64.sh

--- targets/armvirt64.sh
+++ targets/armvirt64.sh
@@ -23,7 +23,7 @@ download_imagebuilder () {
     tar -xJf immortalwrt-imagebuilder-* && rm -f immortalwrt-imagebuilder-*.tar.xz
     mv -f immortalwrt-imagebuilder-* ${openwrt_dir}
 #    mv -f custom-files/repositories.conf ${imagebuilder_path}
-    sed -i "s|CONFIG_TARGET_ROOTFS_PARTSIZE=104|CONFIG_TARGET_ROOTFS_PARTSIZE=800|g" ${imagebuilder_path}/.config || error_msg
+    sed -i "s|CONFIG_TARGET_ROOTFS_PARTSIZE=300|CONFIG_TARGET_ROOTFS_PARTSIZE=800|g" ${imagebuilder_path}/.config || error_msg
     sed -i "s/^option check_signature/#option check_signature/g" ${imagebuilder_path}/repositories.conf || error_msg
     sed -i "s/^CONFIG_MODULE_DEFAULT_libustream-openssl=y/#CONFIG_MODULE_DEFAULT_libustream-openssl=y/g" ${imagebuilder_path}/.config|| error_msg
     sed -i "s/^CONFIG_DEFAULT_libustream-openssl=y/#CONFIG_DEFAULT_libustream-openssl=y/g" ${imagebuilder_path}/.config|| error_msg
